6a7bae8ac17774d16d604323135c10b541866a47
Pelle <pelle.soderberg72@gmail.com>
2021-03-17 07:11:49 +0100

    First commit

---
+++ Calc/src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,54 @@
+package stepDefinitions;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.cucumber.java.en.*;
+
+public class StepDefinitions {
+	    
+	private WebDriver driver;
+		
+		@Given("I have entered page www.marshu.com\\/articles\\/calculate-addition-calculator-add-two-numbers.php")
+		public void i_have_entered_page_www_marshu_com_articles_calculate_addition_calculator_add_two_numbers_php() {
+		    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
+			driver = new ChromeDriver();
+		    driver.get("https://www.marshu.com/articles/calculate-addition-calculator-add-two-numbers.php");
+		    driver.manage().window().maximize();
+		    }
+		@Given("I have entered {int}")
+		public void i_have_entered(Integer int1) throws InterruptedException {
+	//	    Thread.sleep(1000); 
+			WebElement num1Box = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/form/p[1]/input"));
+		    num1Box.sendKeys(String.valueOf(int1));
+		}
+		@Given("I have also entered {int}")
+		public void i_have_also_entered(Integer num2) {
+			WebElement num2Box = driver.findElement(By.name("n02"));
+		    num2Box.sendKeys(String.valueOf(num2));
+		}
+		@When("I press add")
+		public void i_press_add() throws InterruptedException {
+	//		Thread.sleep(1000);
+			WebElement addBox = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/form/p[4]/input"));
+			addBox.click();
+		}
+		@Then("the result should be {int}")
+		public void the_result_should_be(Integer int1) throws InterruptedException {
+			String result;
+			WebElement resultBox = driver.findElement(By.name("answer"));
+			result=resultBox.getAttribute("value");
+			assertEquals(String.valueOf(int1),result);
+	//		Thread.sleep(2000);
+		    driver.quit();
+		}
+
+
+
+}
---
+++ Calc/src/test/resources/Features/Calc.feature
@@ -0,0 +1,27 @@
+Feature: Cal
+I need an calculatior so I do not do silly misstake
+Background:
+Given I have entered page www.marshu.com/articles/calculate-addition-calculator-add-two-numbers.php
+
+@add
+Scenario: Add two numbers
+Given I have entered 70
+And I have also entered 51
+When I press add 
+Then the result should be 121
+
+Scenario Outline: Add two numbers
+Given I have entered <num1>
+And I have also entered <num2>
+When I press add 
+Then the result should be <result>
+Examples:
+|num1|num2|result|
+|1|-1|0|
+|2|-1|1|
+|3|-1|2|
+|4|-1|3|
+|5|-1|4|
+|6|-1|5|
+|7|-1|6|
+|8|-1|7|
